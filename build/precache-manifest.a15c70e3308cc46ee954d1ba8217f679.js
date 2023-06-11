b7a029f372f42107ebf6c613d1c1b2203d0b676b
Vusi Mngomezulu <vusimn@Vusis-Air.home>
2023-06-11 22:45:19 +0200

    new blog with audio

---
+++ build/precache-manifest.a15c70e3308cc46ee954d1ba8217f679.js
@@ -0,0 +1,274 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ef1edbfa2c81b7a0476c9843dde1b64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ccbdc2a5e3b982fa472",
+    "url": "/static/css/main.ab095cc1.chunk.css"
+  },
+  {
+    "revision": "ec4a25605b7c2e7844ad",
+    "url": "/static/js/2.dcba86e3.chunk.js"
+  },
+  {
+    "revision": "9ccbdc2a5e3b982fa472",
+    "url": "/static/js/main.37f114a7.chunk.js"
+  },
+  {
+    "revision": "4b9b94e173029875c5aa",
+    "url": "/static/js/runtime~main.bf647e24.js"
+  },
+  {
+    "revision": "5c7854941faf1fa126bd30f19cffddf0",
+    "url": "/static/media/1.5c785494.png"
+  },
+  {
+    "revision": "69a3219891d57ced73ec18dee798ecd1",
+    "url": "/static/media/1.69a32198.png"
+  },
+  {
+    "revision": "7a0c7e0140c0b252b55febc4d0365cde",
+    "url": "/static/media/1.7a0c7e01.png"
+  },
+  {
+    "revision": "3d3c6e4f80cccf189a41aefac536e4e1",
+    "url": "/static/media/2.3d3c6e4f.png"
+  },
+  {
+    "revision": "ffc1f6a2eed6262cd2251e754cb0c784",
+    "url": "/static/media/2.ffc1f6a2.png"
+  },
+  {
+    "revision": "2200c942299ec303c7117c68ad5faa87",
+    "url": "/static/media/3.2200c942.png"
+  },
+  {
+    "revision": "25002283215cd578758d75e4ee58485b",
+    "url": "/static/media/3.25002283.png"
+  },
+  {
+    "revision": "b2de9cb917519e373a7f2a304b669f50",
+    "url": "/static/media/3.b2de9cb9.png"
+  },
+  {
+    "revision": "0db8eee77897f229665cf9ea6eea9635",
+    "url": "/static/media/4.0db8eee7.png"
+  },
+  {
+    "revision": "47d6449bc8da0fd5a834d90e73fd810d",
+    "url": "/static/media/4.47d6449b.png"
+  },
+  {
+    "revision": "6f6b42c4702d93a5d71306a37c691a8b",
+    "url": "/static/media/4.6f6b42c4.png"
+  },
+  {
+    "revision": "2983b43f0976975dc7fc9d2affd8a8e2",
+    "url": "/static/media/5.2983b43f.png"
+  },
+  {
+    "revision": "317c3334d9de4d5e971d1755eac35958",
+    "url": "/static/media/5.317c3334.png"
+  },
+  {
+    "revision": "87ee422a725ccab412a538dac8631326",
+    "url": "/static/media/6.87ee422a.png"
+  },
+  {
+    "revision": "ac7ca208cd1d8f3636c0bb45a758cbd1",
+    "url": "/static/media/BlackBarbie1.ac7ca208.jpg"
+  },
+  {
+    "revision": "918c2741cc012f7ea0e2fa07b96021ab",
+    "url": "/static/media/BlackBarbie2.918c2741.jpg"
+  },
+  {
+    "revision": "7613584252fee95684d5e676935b99a4",
+    "url": "/static/media/GazaBanner.76135842.png"
+  },
+  {
+    "revision": "7537b487e326d864ee5a473b6b0bf05f",
+    "url": "/static/media/KalushiBanner.7537b487.png"
+  },
+  {
+    "revision": "3ae1ec2701487dadea0ea4f2830ca800",
+    "url": "/static/media/KalushiBanner1.3ae1ec27.jpeg"
+  },
+  {
+    "revision": "57cb8b49316a7406fca1dc8e9842d115",
+    "url": "/static/media/KalushiBanner2.57cb8b49.jpeg"
+  },
+  {
+    "revision": "fcc794e1f9c4e5a7bb2066090d5ac386",
+    "url": "/static/media/KizitoProfilePic.fcc794e1.png"
+  },
+  {
+    "revision": "0a81892aa5ec29c700ab6a5a51750c2d",
+    "url": "/static/media/Marikana1.0a81892a.jpeg"
+  },
+  {
+    "revision": "5717bef90dcc0a4643cbe90a89dd9f49",
+    "url": "/static/media/Marikana1.5717bef9.jpeg"
+  },
+  {
+    "revision": "d3f317be1974059385f18cd3623e996e",
+    "url": "/static/media/Marikana2.d3f317be.jpg"
+  },
+  {
+    "revision": "ddf7e20324f9a0b6fb9fc9d40844be0b",
+    "url": "/static/media/Trod.ddf7e203.JPG"
+  },
+  {
+    "revision": "a107205e9623825a4fd77d2c9207591c",
+    "url": "/static/media/VusiProfile.a107205e.jpeg"
+  },
+  {
+    "revision": "564fd4e88244f8b61f4697265ff62061",
+    "url": "/static/media/WombMan1.564fd4e8.JPG"
+  },
+  {
+    "revision": "a0052bb9a3f5ed4ec6bf0221646a3b73",
+    "url": "/static/media/WombMan2.a0052bb9.JPG"
+  },
+  {
+    "revision": "f96c461557b563be204468343e66a363",
+    "url": "/static/media/WombManBanner.f96c4615.JPG"
+  },
+  {
+    "revision": "db075b221db4e668927f344e0cb9f2a7",
+    "url": "/static/media/WombManProfile.db075b22.JPG"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e1bc3d575e92a8a9942c8be9bb24f7d9",
+    "url": "/static/media/banner.e1bc3d57.png"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "8a38107a1405aa756f1eea4f2d45c395",
+    "url": "/static/media/biddy.8a38107a.jpg"
+  },
+  {
+    "revision": "3522c4cc9ce082e1fc1e18b6d8fc3e6d",
+    "url": "/static/media/bioscope1.3522c4cc.jpg"
+  },
+  {
+    "revision": "23ffbd85475216332711946464a14c18",
+    "url": "/static/media/blackbarbie.23ffbd85.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "27e1d643a0af6ddb850d627077bf53d9",
+    "url": "/static/media/filmLanding.27e1d643.jpg"
+  },
+  {
+    "revision": "81f12103538b3dbaf86d65986a94aa76",
+    "url": "/static/media/happy.81f12103.jpeg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "c4545d6b3cb5ba47f420caf5f3abe8f3",
+    "url": "/static/media/logo.c4545d6b.png"
+  },
+  {
+    "revision": "0e6a04cfe3d3975804f8cc69bcd8d3a6",
+    "url": "/static/media/mandlaDubeProfile.0e6a04cf.jpg"
+  },
+  {
+    "revision": "eaeb3c1391de191984a2ea276ea7fca3",
+    "url": "/static/media/marikana.eaeb3c13.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "22aa3ad4965639d244c3097f838bbcae",
+    "url": "/static/media/mesete.22aa3ad4.png"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "0b8e60fc91374a1b2a4b731d8b96d1a3",
+    "url": "/static/media/pheeha.0b8e60fc.png"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
